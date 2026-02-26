b538a25d3fd071b9ee870cde6f4914addaca05fa
binger <gyu42@my.bcit.ca>
2026-02-25 19:07:29 -0800

    Deleted test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-devices = tf.config.list_physical_devices()
-print(devices)
-
-# You should see 'GPU' in the list if it's working
-if tf.config.list_physical_devices('GPU'):
-    print("TensorFlow is using the Mac GPU!")
